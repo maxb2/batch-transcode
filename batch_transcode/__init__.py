675c10a137fd75a4185dd685986dd55238355e26
maxb2 <matt@manderscience.com>
2023-04-30 15:05:03 -0500

    feat: typer

---
+++ batch_transcode/__init__.py
@@ -0,0 +1,193 @@
+import os
+from glob import iglob
+from pathlib import Path
+from subprocess import run, CompletedProcess
+from typing import Generator
+
+from dataclasses import dataclass
+from enum import Enum
+
+from pymediainfo import MediaInfo
+
+NICE_BIN = "/usr/bin/nice"
+FFMPEG_BIN = os.environ.get("FFMPEG_BIN", "/usr/bin/ffmpeg")
+
+
+@dataclass
+class VideoFiles:
+    video: Path
+    vcs: Path
+
+    @staticmethod
+    def new(video: Path) -> "VideoFiles":
+        video = Path(video)
+        return VideoFiles(video=video, vcs=video.with_suffix(".jpg"))
+
+
+@dataclass
+class TranscodeFiles:
+    input: VideoFiles
+    output: VideoFiles
+
+    @staticmethod
+    def new(video_in: Path) -> "TranscodeFiles":
+        video_in = Path(video_in)
+        video_out = video_in.with_suffix(".x265.mp4")
+        return TranscodeFiles(
+            input=VideoFiles.new(video_in), output=VideoFiles.new(video_out)
+        )
+
+
+@dataclass
+class TranscodeResult:
+    files: TranscodeFiles
+    result: CompletedProcess
+
+    def is_ok(self) -> bool:
+        return self.result.returncode == 0
+
+
+@dataclass
+class VcsResult:
+    video: VideoFiles
+    result: CompletedProcess
+
+    def is_ok(self) -> bool:
+        return self.result.returncode == 0
+
+
+def get_videos(directory: Path, recursive=False) -> Generator:
+    """Get all video files in a directory
+
+    Parameters
+    ----------
+    directory : Path
+        input directory
+    recursive : bool, optional
+        recurse sub-directories, by default False
+
+    Yields
+    ------
+    Generator
+        generator of video files
+    """
+
+    # TODO: support more file extensions, case insensitivity
+    pattern = f"{directory}/**/*.mp4"
+
+    return iglob(pattern, recursive=recursive)
+
+
+def is_hevc(media_file: Path) -> bool:
+    """Whether media file is HEVC encoded.
+
+    Parameters
+    ----------
+    media_file : Path
+        media file to check
+
+    Returns
+    -------
+    bool
+        whether file is HEVC
+    """
+    return MediaInfo.parse(media_file).video_tracks[0].codec_id in ["hev1", "hvc1"]
+
+
+class HevcDiscriminantMethods(Enum):
+    FAST = "fast"
+    ACCURATE = "accurate"
+    SEMI_ACCURATE = "semi-accurate"
+
+
+def get_non_hevc_mp4(
+    dir: Path,
+    recursive=False,
+    method: HevcDiscriminantMethods = HevcDiscriminantMethods.FAST,
+) -> Generator:
+    """Generate paths to non-HEVC files.
+
+    Parameters
+    ----------
+    dir : str
+        directory to search
+    recursive : bool, optional
+        recurse sub-directories, by default False
+    method : HevcDiscriminantMethods, optional
+        method to determine encoding, by default HevcDiscriminantMethods.FAST
+
+    Yields
+    ------
+    Generator
+        generator of non-hevc files
+    """
+
+    def fast(gen: Generator) -> Generator:
+        return (f for f in gen if not f.endswith("x265.mp4"))
+
+    def accurate(gen: Generator) -> Generator:
+        return (f for f in gen if not is_hevc(f))
+
+    all_vids = get_videos(dir, recursive=recursive)
+
+    if method == HevcDiscriminantMethods.FAST:
+        return fast(all_vids)
+    elif method == HevcDiscriminantMethods.ACCURATE:
+        return accurate(all_vids)
+    elif method == HevcDiscriminantMethods.SEMI_ACCURATE:
+        return accurate(fast(all_vids))
+    else:
+        raise Exception(f"Got a bad value for method: '{method}'")
+
+
+def transcode_vid(files: TranscodeFiles, run_check=True) -> TranscodeResult:
+    """Transcode video using FFMpeg
+
+    Parameters
+    ----------
+    files : TranscodeFiles
+        file to transcode
+
+    Returns
+    -------
+    TranscodeResult
+        transcode results
+    """
+
+    cmd = [
+        NICE_BIN,
+        "--adjustment=20",
+        FFMPEG_BIN,
+        "-hide_banner",
+        "-n",
+        "-i",
+        str(files.input.video),
+        "-c:v",
+        "libx265",
+        "-c:a",
+        "aac",
+        "-strict",
+        "-2",
+        str(files.output.video),
+    ]
+
+    result = run(cmd, check=run_check, capture_output=False)
+    return TranscodeResult(files, result)
+
+
+def make_contact_sheet(video: VideoFiles, run_check: bool = True) -> VcsResult:
+    """Make contact sheet for video file.
+
+    Parameters
+    ----------
+    video : VideoFiles
+        video files
+
+    Returns
+    -------
+    VcsResult
+        vcs result
+    """
+    cmd = f'nice --adjustment=20 vcsi "{video.video}" -t -w 850 -g 3x5 -o "{video.vcs}"'
+    result = run(cmd, shell=True, check=run_check)
+    return VcsResult(video, result)
---
+++ batch_transcode/cli.py
@@ -0,0 +1,144 @@
+import os
+from glob import iglob
+from pathlib import Path
+
+import typer
+
+from batch_transcode import (
+    get_non_hevc_mp4,
+    get_videos,
+    make_contact_sheet,
+    transcode_vid,
+    TranscodeFiles,
+    VideoFiles,
+    HevcDiscriminantMethods,
+)
+
+app = typer.Typer(
+    help="""A cli to transcode video files and make video contact sheets.
+
+    NOTE: currently only supports transcoding to x265. 
+"""
+)
+
+
+@app.command(help="Transcode video file.")
+def transcode(
+    video_path: Path = typer.Argument(..., help="Video file to transcode."),
+    delete_source: bool = typer.Option(
+        False, "-d", "--delete-source", help="Delete source file after transcoding."
+    ),
+):
+    result = transcode_vid(TranscodeFiles.new(video_path))
+
+    if delete_source:
+        typer.echo(f"Removing {result.files.input}...")
+        raise NotImplementedError
+        os.remove(result.files.input.video)
+        os.remove(result.files.input.vcs)
+
+
+@app.command(help="Create video contact sheet.")
+def vcs(
+    video_path: Path = typer.Argument(
+        ..., help="Video file or folder for which to create contact sheets."
+    ),
+    recursive: bool = typer.Option(
+        False, "-r", "--recursive", help="Recurse into subdirectories."
+    ),
+    overwrite: bool = typer.Option(
+        False, "-o", "--overwrite", help="Replace existing files."
+    ),
+):
+    video_path = Path(video_path)
+
+    if video_path.is_file():
+        make_contact_sheet(VideoFiles.new(video_path))
+
+    elif video_path.is_dir():
+        for vid in get_videos(video_path, recursive=recursive):
+            video_files = VideoFiles.new(vid)
+
+            if not overwrite and video_files.vcs.is_file():
+                continue
+
+            make_contact_sheet(video_files)
+    else:
+        raise Exception("Path or file doesn't exist: {video_path}")
+
+
+@app.command(help="Batch transcoding.")
+def batch(
+    dir: Path = typer.Argument(..., help="Directory containing videos to transcode."),
+    recursive: bool = typer.Option(
+        False, "-r", "--recursive", help="Recurse into subdirectories."
+    ),
+    # replace_existing: bool = typer.Option(False, help="Replace existing files."),
+    # vcs_only: bool = typer.Option(False, help="Only make vidoe contact sheets."),
+    move_source: Path = typer.Option(
+        None, "-m", "--move-source", help="Move source files after transcoding."
+    ),
+    delete_source: bool = typer.Option(
+        False, "-d", "--delete-source", help="Delete source files after transcoding."
+    ),
+):
+    if delete_source and move_source is not None:
+        typer.echo(
+            "Cannot use --delete-source/-d and --move-source/-m at the same time.",
+            color="yellow",
+            err=True,
+        )
+        raise typer.Abort
+
+    if move_source is not None and not move_source.is_dir():
+        raise Exception(f"{move_source} is not an existing directory!")
+
+    to_convert = get_non_hevc_mp4(dir, recursive=recursive)
+
+    for vid_path in to_convert:
+        transcode_files = TranscodeFiles.new(vid_path)
+
+        if not transcode_files.output.video.is_file():
+            transcode_result = transcode_vid(transcode_files)
+            make_contact_sheet(transcode_result.files.output)
+
+        if move_source is not None:
+            typer.echo(f"Moving {transcode_result.files.input} to {move_source}...")
+            # raise NotImplementedError
+            moved_video = VideoFiles.new(
+                move_source.joinpath(transcode_result.files.input.video.name)
+            )
+            os.rename(transcode_result.files.input.video, moved_video.video)
+            if transcode_result.files.input.vcs.is_file():
+                os.rename(transcode_result.files.input.vcs, moved_video.vcs)
+
+        if delete_source:
+            typer.echo(f"Removing {transcode_files}...")
+            raise NotImplementedError
+            os.remove(transcode_result.files.input.video)
+            os.remove(transcode_result.files.input.vcs)
+
+
+@app.command()
+def find(
+    dir: Path = typer.Argument(...),
+    recursive: bool = typer.Option(
+        False, "-r", "--recursive", help="Recurse into subdirectories."
+    ),
+    method: HevcDiscriminantMethods = typer.Option(
+        HevcDiscriminantMethods.FAST.value,
+        help="Method for determining if the file is HEVC encoded.",
+    ),
+    limit: int = typer.Option(None, help="Limit the number of files to print."),
+):
+    vids = get_non_hevc_mp4(dir, recursive=recursive, method=method)
+    _iter = 1
+    for vid in vids:
+        if limit is not None and _iter > limit:
+            break
+        typer.echo(vid)
+        _iter += 1
+
+
+if __name__ == "__main__":
+    app()
